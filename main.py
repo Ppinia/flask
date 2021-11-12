e9d53a332792da9ebbbabe656e5539ee67269d7a
Marta Stimec <marta.dovic@gmail.com>
2021-11-12 17:23:18 +0100

    flask and jinja

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/pletemsi")
+@app.route("/pletemsi", methods=["GET", "POST"])
 def pletemsi():
     return render_template("pletemsi.html")
 
@@ -10,6 +10,19 @@ def pletemsi():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
